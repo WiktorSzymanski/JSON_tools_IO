56790713f7de7d3c52f8cb5de07cdd86c100f8d4
Jakub Czajka <CzajkaPL@o2.pl>
2022-12-11 21:19:36 +0100

    story1 | Zaimplementować API odsyłające JSON-a wysłanego przez użytkownika#7

--- src/main/java/pl/put/poznan/JSON_tools/logic/JsonTools.java
+++ src/main/java/pl/put/poznan/JSON_tools/logic/JsonTools.java
@@ -3,11 +3,11 @@ package pl.put.poznan.JSON_tools.logic;
 /**
  * This is just an example to show that the logic should be outside the REST service.
  */
-public class TextTransformer {
+public class JsonTools {
 
     private final String[] transforms;
 
-    public TextTransformer(String[] transforms){
+    public JsonTools(String[] transforms){
         this.transforms = transforms;
     }
 
