1b0a74a660ee66d90a0cecd37f263f99654887f4
WiktorSzymanski <43135532+WiktorSzymanski@users.noreply.github.com>
2022-12-20 18:16:04 +0000

    Json tools #16 (#23)
    * Add: abstract class JSONDecorator that will extend other decorators
    
    * Fix: added method to JSONDecorator and deleted unused import
    
    Co-authored-by: BartoszDolata <97393249+BartoszDolata@users.noreply.github.com>

--- src/main/java/pl/put/poznan/JSON_tools/logic/JSONDecorator.java
+++ src/main/java/pl/put/poznan/JSON_tools/logic/JSONDecorator.java
@@ -1,13 +1,19 @@
 package pl.put.poznan.JSON_tools.logic;
 
-import lombok.Getter;
+
 import org.json.JSONObject;
 
-@Getter
+
 public abstract class JSONDecorator extends JSONObject{
-    private JSONObject jsonObject;
+    protected JSONObject jsonObject;
+
 
     public JSONDecorator( JSONObject jsonObject ) {
         this.jsonObject = jsonObject;
     }
+
+    public String getJSON() {
+        return jsonObject.toString();
+    }
+    
 }
