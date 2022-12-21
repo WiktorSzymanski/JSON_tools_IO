8e63ce959dc5370f8b14fadba5bca82605876a0a
Jakub Czajka <CzajkaPL@o2.pl>
2022-12-21 14:18:14 +0100

    resolved conf

--- src/main/java/pl/put/poznan/JSON_tools/test/JSONFilterTest.java
+++ src/main/java/pl/put/poznan/JSON_tools/test/JSONFilterTest.java
@@ -1,32 +1,44 @@
 package pl.put.poznan.JSON_tools.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
-import pl.put.poznan.JSON_tools.logic.JSONDecorator;
+
 import pl.put.poznan.JSON_tools.logic.JSONFilter;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class JSONFilterTest {
+class JSONFilterTest
+{
 
     @Test
-    void testFiltering() {
-        JSONObject json = new JSONObject("{one: two, key: value}");
-        JSONFilter jf = new JSONFilter(json, new String[] {"key"});
-        assertEquals("{\"key\":\"value\"}", jf.getJSON().toString());
+    void testFiltering()
+    {
+        JSONObject json = new JSONObject( "{one: two, key: value}" );
+        JSONFilter jf = new JSONFilter( json, new String[]
+        { "key" } );
+        assertEquals( "{\"key\":\"value\"}", jf.getJSON()
+            .toString() );
     }
+
     @Test
-    void testFiltering2() {
-        JSONObject json = new JSONObject("{one: two, key: value}");
-        JSONFilter jf = new JSONFilter(json, new String[] {"key", "one"});
-        assertEquals("{\"one\":\"two\",\"key\":\"value\"}", jf.getJSON().toString());
+    void testFiltering2()
+    {
+        JSONObject json = new JSONObject( "{one: two, key: value}" );
+        JSONFilter jf = new JSONFilter( json, new String[]
+        { "key", "one" } );
+        assertEquals( "{\"one\":\"two\",\"key\":\"value\"}", jf.getJSON()
+            .toString() );
 
     }
+
     @Test
-    void testFiltering3() {
-        JSONObject json = new JSONObject("{one: two, key: value}");
-        JSONFilter jf = new JSONFilter(json, new String[] {});
-        assertEquals("{}", jf.getJSON().toString());
+    void testFiltering3()
+    {
+        JSONObject json = new JSONObject( "{one: two, key: value}" );
+        JSONFilter jf = new JSONFilter( json, new String[]
+        {} );
+        assertEquals( "{}", jf.getJSON()
+            .toString() );
 
     }
 }
